3e96c4b3ee109bd392562dc743c112acf5e7eaab
Nnenanya Obinna <obinnaeye@yahoo.com>
2016-05-18 21:51:43 +0100

    Add test for returning name of Author

---
+++ ooptest.spec.js
@@ -0,0 +1,9 @@
+// This is a test code for "bootday2.js" in c/mybootcamp
+
+var app = new NotesApplication();
+
+describe("Author's Name", function(){
+	it('Should return the Name of Author', function(){
+		expect(NotesApplication(Author)).toBe("Author");
+	});
+});
