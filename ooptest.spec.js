ea7cc3eac03275fa5f4031fb8a4984d2722e6d0d
Nnenanya Obinna <obinnaeye@yahoo.com>
2016-05-18 23:21:15 +0100

    Add test for adding generating list

--- ooptest.spec.js
+++ ooptest.spec.js
@@ -1,18 +1,16 @@
 // This is a test code for "bootday2.js" in c/mybootcamp
 
-var app = new NotesApplication();
-
 describe("Author's Name", function(){
-	it('Should return the Name of Author', function(){
+	//it('Should return the Name of Author', function(){
 		expect(NotesApplication("Author")).toBe("Author");
 	});
 
-	it('Should return a string', function(){
+	it('Should not accept non strings', function(){
 		expect(NotesApplication.prototype.create(5)).toBe("Note content should be strigs. Try again");
 	});
 
-	//it('Should return a string', function(){
-	//	expect(NotesApplication.prototype.create(5)).toBe("Note content should be strigs. Try again");
-	//});
+	it('Should be a function', function(){
+		expect(typeof NotesApplication.prototype.listNotes).toBe(typeof (function){});
+	});
 
 });
