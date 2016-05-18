373e2242dd3bc276706ee1cd96c5fbfd62c6352f
Nnenanya Obinna <obinnaeye@yahoo.com>
2016-05-18 22:43:02 +0100

    Add test for adding content to list

--- ooptest.spec.js
+++ ooptest.spec.js
@@ -6,4 +6,13 @@ describe("Author's Name", function(){
 	it('Should return the Name of Author', function(){
 		expect(NotesApplication("Author")).toBe("Author");
 	});
+
+	it('Should return a string', function(){
+		expect(NotesApplication.prototype.create(5)).toBe("Note content should be strigs. Try again");
+	});
+
+	//it('Should return a string', function(){
+	//	expect(NotesApplication.prototype.create(5)).toBe("Note content should be strigs. Try again");
+	//});
+
 });
