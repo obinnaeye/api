d585ad525282fc039e07a5d5111ffe488a6e49b2
Nnenanya Obinna <obinnaeye@yahoo.com>
2016-05-19 07:20:47 +0100

    Add test for search and edit function

--- ooptest.spec.js
+++ ooptest.spec.js
@@ -1,16 +1,24 @@
 // This is a test code for "bootday2.js" in c/mybootcamp
 
 describe("Author's Name", function(){
-	//it('Should return the Name of Author', function(){
+	it('Should return the Name of Author', function(){
 		expect(NotesApplication("Author")).toBe("Author");
 	});
 
-	it('Should not accept non strings', function(){
-		expect(NotesApplication.prototype.create(5)).toBe("Note content should be strigs. Try again");
+	it('Should return the Name of Author', function(){
+		expect(NotesApplication("Author")).toBe("Author");
+
+	it('Should not accept non strings for search item', function(){
+		expect(NotesApplication.prototype.search(5)).toBe("Search item should be string. Please try again");
 	});
 
 	it('Should be a function', function(){
-		expect(typeof NotesApplication.prototype.listNotes).toBe(typeof (function){});
+		expect(typeof(NotesApplication.prototype.create)).toBe(typeof(function(){}));
 	});
 
+	it('Should be a function: edit', function(){
+		expect(typeof(NotesApplication.prototype.edit)).toBe(typeof(function(){}));
+	})
+
+	
 });
